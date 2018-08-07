d642cf3a0e163dbc63f1be689f8ba03c8f4fbba6
lls-laptop <2247407869@qq.com>
2018-08-07 11:51:22 +0800

    加入前端页面，解决跨域问题

--- src/main/java/com/lls/springboot/web/CategoryController.java
+++ src/main/java/com/lls/springboot/web/CategoryController.java
@@ -8,7 +8,11 @@ import com.lls.springboot.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class CategoryController {
@@ -16,7 +20,11 @@ public class CategoryController {
     CategoryMapper categoryMapper;
 
     @RequestMapping(value = "/listCategory")
-    public PageInfo listCategory(int pageNum, int pageSize) {
+    @ResponseBody
+    public PageInfo listCategory(HttpServletResponse response,
+                                 @RequestParam("pageNum") int pageNum,
+                                 @RequestParam("pageSize") int pageSize) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         PageHelper.startPage(pageNum, pageSize);
         List<Category> list = categoryMapper.findAll();
         PageInfo pageInfo = new PageInfo(list);
