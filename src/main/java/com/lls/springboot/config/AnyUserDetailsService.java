1c8088688411d3a710b4c9da9329ca34145223d8
stormynight <2247407869@qq.com>
2018-09-29 22:36:44 +0800

    spring security研究中

--- src/main/java/com/lls/springboot/config/AnyUserDetailsService.java
+++ src/main/java/com/lls/springboot/config/AnyUserDetailsService.java
@@ -1,52 +1,52 @@
-//package com.lls.springboot.config;
-//
-//import com.lls.springboot.pojo.UserEntity;
-//import com.lls.springboot.service.IUserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Service
-//public class AnyUserDetailsService implements UserDetailsService {
-//
-//    private final IUserService IUserService;
-//
-//    @Autowired
-//    AnyUserDetailsService(IUserService IUserService){
-//        this.IUserService = IUserService;
-//    }
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        UserEntity userEntity = IUserService.getByUsername(username);
-//        if (userEntity == null){
-//            throw new UsernameNotFoundException("用户不存在！");
-//        }
-//        List<SimpleGrantedAuthority> simpleGrantedAuthorities = createAuthorities(userEntity.getRole());
-//        return new User(userEntity.getUsername(), userEntity.getPassword(), simpleGrantedAuthorities);
-//    }
-//
-//    /**
-//     * 权限字符串转化
-//     *
-//     * 如 "USER,ADMIN" -> SimpleGrantedAuthority("USER") + SimpleGrantedAuthority("ADMIN")
-//     *
-//     * @param roleStr 权限字符串
-//     */
-//    private List<SimpleGrantedAuthority> createAuthorities(String roleStr){
-//        String[] roles = roleStr.split(",");
-//        List<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
-//        for (String role : roles) {
-//            simpleGrantedAuthorities.add(new SimpleGrantedAuthority(role));
-//        }
-//        return simpleGrantedAuthorities;
-//    }
-//
-//}
+package com.lls.springboot.config;
+
+import com.lls.springboot.model.UserPo;
+import com.lls.springboot.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AnyUserDetailsService implements UserDetailsService {
+
+    private final IUserService userService;
+
+    @Autowired
+    AnyUserDetailsService(IUserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserPo userPo = IUserService.getByUsername(username);
+        if (userPo == null){
+            throw new UsernameNotFoundException("用户不存在！");
+        }
+        List<SimpleGrantedAuthority> simpleGrantedAuthorities = createAuthorities(userPo.getRoles());
+        return new User(userPo.getUsername(), userPo.getPassword(), simpleGrantedAuthorities);
+    }
+
+    /**
+     * 权限字符串转化
+     *
+     * 如 "USER,ADMIN" -> SimpleGrantedAuthority("USER") + SimpleGrantedAuthority("ADMIN")
+     *
+     * @param roleStr 权限字符串
+     */
+    private List<SimpleGrantedAuthority> createAuthorities(String roleStr){
+        String[] roles = roleStr.split(",");
+        List<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
+        for (String role : roles) {
+            simpleGrantedAuthorities.add(new SimpleGrantedAuthority(role));
+        }
+        return simpleGrantedAuthorities;
+    }
+
+}
--- src/main/java/com/lls/springboot/service/impl/UserService.java
+++ src/main/java/com/lls/springboot/service/impl/UserService.java
@@ -1,44 +1,44 @@
-//package com.lls.springboot.service.impl;
-//
-//import com.lls.springboot.mapper.IUserMapper;
-//import com.lls.springboot.pojo.UserEntity;
-//import com.lls.springboot.service.IUserService;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@Primary
-//public class UserService implements IUserService {
-//
-//    private final IUserMapper userMapper;
-//
-//    public UserService(IUserMapper userMapper){
-//        this.userMapper = userMapper;
-//    }
-//
-//    @Override
-//    public boolean insert(UserEntity userEntity) {
-//        String username = userEntity.getUsername();
-//        if (exist(username))
-//            return false;
-//        userEntity.setRole("ROLE_USER");
-//        int result = userMapper.insert(userEntity);
-//        return  result == 1;
-//    }
-//
-//    @Override
-//    public UserEntity getByUsername(String username) {
-//        return userMapper.selectByUsername(username);
-//    }
-//
-//    /**
-//     * 判断用户是否存在
-//     * @param username 账号
-//     * @return 密码
-//     */
-//    private boolean exist(String username){
-//        UserEntity userEntity = userMapper.selectByUsername(username);
-//        return (userEntity != null);
-//    }
-//
-//}
+package com.lls.springboot.service.impl;
+
+import com.lls.springboot.dao.IUserDao;
+import com.lls.springboot.model.UserPo;
+import com.lls.springboot.service.IUserService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class UserService implements IUserService {
+
+    private final IUserDao userDao;
+
+    public UserService(IUserDao userDao){
+        this.userDao = userDao;
+    }
+
+    @Override
+    public boolean insert(UserPo userPo) {
+        String username = userPo.getUsername();
+        if (exist(username))
+            return false;
+        userPo.setRoles("ROLE_USER");
+        int result = userDao.insert(userPo);
+        return  result == 1;
+    }
+
+    @Override
+    public UserPo getByUsername(String username) {
+        return userDao.selectByUsername(username);
+    }
+
+    /**
+     * 判断用户是否存在
+     * @param username 账号
+     * @return 密码
+     */
+    private boolean exist(String username){
+        UserPo userPo = userDao.selectByUsername(username);
+        return (userPo != null);
+    }
+
+}
