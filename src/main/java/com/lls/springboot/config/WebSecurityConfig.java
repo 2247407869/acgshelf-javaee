b1713747fea72696259ca432057b84e6ac61831f
stormynight <2247407869@qq.com>
2018-10-22 21:59:07 +0800

    用户可以注册了

--- src/main/java/com/lls/springboot/config/WebSecurityConfig.java
+++ src/main/java/com/lls/springboot/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.lls.springboot.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
@@ -39,4 +41,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(AuthenticationManagerBuilder builder) throws Exception{
         builder.userDetailsService(anyUserDetailsService);
     }
+
+//    /**
+//     * 密码加密
+//     */
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
 }
